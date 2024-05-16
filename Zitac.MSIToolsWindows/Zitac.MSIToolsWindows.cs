b1d766e1346d403fe299ed038b93411087f47be6
Kenny Vigersand <kenny.vigersand@zitac.se>
2024-05-16 21:49:34 +0200

    Added Windows Version

--- Zitac.MSIToolsWindows/Zitac.MSIToolsWindows.cs
+++ Zitac.MSIToolsWindows/Zitac.MSIToolsWindows.cs
@@ -1,5 +1,5 @@
 ﻿using DecisionsFramework.Design.Flow;
-using System.Diagnostics;
+using WixToolset.Dtf.WindowsInstaller;
 
 namespace Zitac.MSIToolsWindows;
 
@@ -9,5 +9,36 @@ public class MSIToolsWindows
 
     public string GetMSIVersionWindows(byte[] FileContent)
     {
+                    // Create a temporary file to write the byte array to.
+            string tempFilePath = Path.GetTempFileName();
+            File.WriteAllBytes(tempFilePath, FileContent);
+
+            try
+            {
+                using (var database = new Database(tempFilePath, DatabaseOpenMode.ReadOnly))
+                {
+                    string query = "SELECT `Value` FROM `Property` WHERE `Property`='ProductVersion'";
+                    using (var view = database.OpenView(query))
+                    {
+                        view.Execute();
+                        using (var record = view.Fetch())
+                        {
+                            return record.GetString("Value");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Failed to get MSI file version. The error was: {ex.Message}", ex);
+            }
+            finally
+            {
+                // Clean up the temporary file.
+                if (File.Exists(tempFilePath))
+                {
+                    File.Delete(tempFilePath);
+                }
+            }
     }
 }
